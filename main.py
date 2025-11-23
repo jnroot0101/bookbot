7513b6ebb24dfe60d804dc58c4579545d8a06f8d
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2025-11-23 23:47:10 +0100

    finish mini project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_count_chars
+import sys
+
+from stats import sort_data, get_count_chars, get_num_words
 
 
 def get_book_text(file_path):
@@ -7,10 +9,22 @@ def get_book_text(file_path):
 
 
 def main():
-    file_contents = get_book_text('books/frankenstein.txt')
-    # print(file_contents)
-    # get_num_words(file_contents)
-    get_count_chars(file_contents)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_contents = get_book_text(sys.argv[1])
+
+    get_num_words(file_contents)
+    data = get_count_chars(file_contents)
+    result = sort_data(data)
+
+    for item in result:
+        char = item["char"]
+        num = item["num"]
+
+        if char.isalpha():
+            print(f"{char}: {num}")
 
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -1,23 +1,28 @@
+def sort_on(items):
+    return items["num"]
+
+
 def get_num_words(text):
     num_words = len(text.split())
     print(f"Found {num_words} total words")
-    # print(text)
 
 
 def get_count_chars(text: str) -> dict[str, int]:
     freq = {}
-    # texts = text.split()
-    # print(text)
 
     for char in text.lower():
-        # print(char)
-        # char = char.lower()
         freq[char] = freq.get(char, 0) + 1
-        # # freq.get(char, 0)
-        # if char in freq:
-        #     freq[char] += 1
-        # else:
-        #     freq[char] = 1
 
-    print(freq)
     return freq
+
+
+def sort_data(data: dict[str, int]) -> list[dict]:
+    items = [
+        {"char": char, "num": count}
+        for char, count in data.items()
+        # if char.isalpha()  # skip spaces, punctuation, symbols, etc.
+    ]
+
+    items.sort(reverse=True, key=sort_on)
+
+    return items
